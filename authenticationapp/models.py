75108e20931d1f3c78cf49aa7ea114ac0444cd5f
proflamyt <53262578+proflamyt@users.noreply.github.com>
2023-03-28 12:53:16 +0100

    added migrations

--- authenticationapp/models.py
+++ authenticationapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser): 
+    is_editor = models.BooleanField(default=False)
+    profile_img = models.ImageField()
+
+
+
+
+
