93ef6ad9cf3705f9539702cca399d47af98a9d93
proflamyt <53262578+proflamyt@users.noreply.github.com>
2023-04-03 14:06:56 +0100

    added Like to Blog

--- blogapp/urls.py
+++ blogapp/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('date', views.current_datetime)
+    path('date', views.current_datetime),
+    path('blog', views.get_blogs)
 ]
+
+
+
--- blogapp/views.py
+++ blogapp/views.py
@@ -6,4 +6,10 @@ import datetime
 def current_datetime(request):
     now = datetime.datetime.now()
     html = f"<html> <body> the current date and time of this blogpost is {now} </body></html>" # make the html better
-    return HttpResponse(html)
+    return HttpResponse(html, status=201)
+
+
+
+def get_blogs(request):
+
+    return HttpResponse('No Blog ', status=404)
